e201d4f8681dace94e2cb33b0102ae5c7d28de7d
Jonhy Willy <jonhy.willy.jw71@gmail.com>
2022-11-17 15:56:22 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+            <ol class="types">
+                <li class="type">Grass</li>
+                <li class="type">Poison</li>
+            </ol>
+
+            <img
+            src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="${pokemon.name}"
+            />
+        </div>
+    </li>
+    
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
+
+
+
+  
